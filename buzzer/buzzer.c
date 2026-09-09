cc6acba64efaf7c3ed5fc2be4336d2a77de293a5
SalemKhatib <salemkhatib149@gmail.com>
2026-09-10 00:39:29 +0300

    Update buzzer.c

--- buzzer/buzzer.c
+++ buzzer/buzzer.c
@@ -1,22 +1,22 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "driver/gpio.h"
-
 #include "buzzer.h"
 
 #define BUZZER_PIN GPIO_NUM_23
 
-void buzzer_demo(void)
+void buzzer_init(void)
 {
     gpio_reset_pin(BUZZER_PIN);
     gpio_set_direction(BUZZER_PIN, GPIO_MODE_OUTPUT);
 
-    while (1)
-    {
-        gpio_set_level(BUZZER_PIN, 1);
-        vTaskDelay(pdMS_TO_TICKS(500));
+    buzzer_off();
+}
 
-        gpio_set_level(BUZZER_PIN, 0);
-        vTaskDelay(pdMS_TO_TICKS(500));
-    }
+void buzzer_on(void)
+{
+    gpio_set_level(BUZZER_PIN, 1);
+}
+
+void buzzer_off(void)
+{
+    gpio_set_level(BUZZER_PIN, 0);
 }
